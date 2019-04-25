d80f7e289ebd1552e6c83f096c8c03287f6b20e1
MARVIN_BOOK\Marvin <marvin@fam-kubik.de>
2019-04-25 11:44:49 +0200

    Moved two methods to Movie class

--- src/Rental.java
+++ src/Rental.java
@@ -8,29 +8,11 @@ class Rental {
     }
 
     public double getCharge() {
-        double charge = 0;
-        switch (movie.getPriceCode()) {
-            case Movie.REGULAR:
-                charge += 2;
-                if (daysRented > 2)
-                    charge += (daysRented - 2) * 1.5;
-                break;
-            case Movie.NEW_RELEASE:
-                charge += daysRented * 3;
-                break;
-            case Movie.CHILDRENS:
-                charge += 1.5;
-                if (daysRented > 3)
-                    charge += (daysRented - 3) * 1.5;
-                break;
-        }
-        return charge;
+        return movie.getCharge(daysRented);
     }
 
     public int getFrequentRenterPoints() {
-        // add bonus for a two day new release rental
-        if ((movie.getPriceCode() == Movie.NEW_RELEASE) && daysRented > 1) return 2;
-        return 1;
+        return movie.getFrequentRenterPoints(daysRented);
     }
 
     public int getDaysRented() {
